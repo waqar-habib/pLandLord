26a4752fac32e22cf1cbedd919a39136e5d3ffd8
Wes McC <wkmccowin@yahoo.com>
2019-06-15 09:44:15 -0700

    test commit

--- models/units.js
+++ models/units.js
@@ -1,7 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
     var Unit= sequelize.define("Unit", {
-      // Giving the Author model a name of type STRING
-      id: DataTypes.id,
+      id: DataTypes.ID,
       number: DataTypes.INT,
       base_price: DataTypes.INT,
       number_of_rooms: DataTypes.INT,
@@ -10,8 +9,7 @@ module.exports = function(sequelize, DataTypes) {
     });
   
     Unit.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
+
       Unit.hasMany(models.Tenant, {
         onDelete: "cascade"
       });
