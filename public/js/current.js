bc6ef452fb4ae3c12fcd810fd589793ea4064e8a
Wes McC <wkmccowin@yahoo.com>
2019-06-18 15:29:55 -0700

    saving changes before pulling from master

--- public/js/current.js
+++ public/js/current.js
@@ -8,21 +8,21 @@ $(document).ready(function() {
     tenantModelSelect.on("change", handleModelChange);
     var posts;
 
-    function searchTenant() {
-        var tenantString = tenant || "";
-        if (tenantString) {
-          tenantString = "/tenant/" + tenantString;
-        }
-       $.get("/api/tenant" + tenantString, function(data) {
-            console.log("tenants", data);
-            posts = data;
-            if (!tenants || !tenants.length) {
-              displayEmpty();
-            }
-            else {
-              initializeRows();
-            }
-          });
+    function searchTenant(tenantData) {
+        $.get("/api/:tenant", tenantData)
+          .then(createTenantRow);
       }
-
+    
+      // Function for creating a new list row for authors
+      function createTenantRow(authorData) {
+        console.log(tenantData);
+        var newTr = $("<tr>");
+        newTr.data("tenant", tenantData);
+        newTr.append("<td>" + tenantData.name + "</td>");
+        newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
+        newTr.append("<td><a href='/blog?author_id=" + tenantData.id + "'>Go to Posts</a></td>");
+        newTr.append("<td><a href='/cms?author_id=" + tenantData.id + "'>Create a Post</a></td>");
+        newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
+        return newTr;
+      }
     });
