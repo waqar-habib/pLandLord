d38974457f074b8088b721fe2d5d38152b733961
Wes McC <wkmccowin@yahoo.com>
2019-06-18 18:36:58 -0700

    Updated current.js, pushing changes to pull upated code from Waqar

--- public/js/current.js
+++ public/js/current.js
@@ -1,20 +1,57 @@
 $(document).ready(function() {
     // Tenant Container holds all of our information regarding the Tenant searched
+    var nameInput = $("#tenant-name");
+    var tenantList = $("tbody");
     var tenantContainer = $(".tenant-container");
-    var tenantModelSelect = $("#category");
+    var tenantModelSelect = $("#modelselect");
     // Click events for the edit and delete buttons
-    $(document).on("click", "button.search", searchTenant);
+    $(document).on("click", "button.search", tenantSearch);
     $(document).on("click", "button.delete", deleteTenant);
     tenantModelSelect.on("change", handleModelChange);
-    var posts;
+   
 
-    function searchTenant(tenantData) {
-        $.get("/api/:tenant", tenantData)
-          .then(createTenantRow);
+    getTenants();
+
+    function tenantSearch(event) {
+        event.preventDefault();
+        // Don't do anything if the name fields hasn't been filled out
+        if (!nameInput.val().trim().trim()) {
+          return;
+        }
+        // Calling the upsertAuthor function and passing in the value of the name input
+        getTenants({
+          name: nameInput
+            .val()
+            .trim()
+        });
+      }
+    
+
+    function getTenants() {
+        $.get("/api/tenants", function(data) {
+          var rowsToAdd = [];
+          for (var i = 0; i < data.length; i++) {
+            rowsToAdd.push(createTenantRow(data[i]));
+          }
+          renderTenantList(rowsToAdd);
+          nameInput.val("");
+        });
+      }
+
+      function renderTenantList(rows) {
+        tenantList.children().not(":last").remove();
+        tenantContainer.children(".alert").remove();
+        if (rows.length) {
+          console.log(rows);
+          tenantList.prepend(rows);
+        }
+        else {
+          renderEmpty();
+        }
       }
     
       // Function for creating a new list row for authors
-      function createTenantRow(authorData) {
+      function createTenantRow(tenantData) {
         console.log(tenantData);
         var newTr = $("<tr>");
         newTr.data("tenant", tenantData);
