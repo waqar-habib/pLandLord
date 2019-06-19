1430d1fc923d5c7043de5521fdc85f8206b69399
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-06-19 14:03:16 -0500

    alternate syntax for funcs on current.js since the one on master branch isn't working. This syntax is not working but will fix it

--- public/js/current.js
+++ public/js/current.js
@@ -5,61 +5,64 @@ $(document).ready(function() {
     var tenantContainer = $(".tenant-container");
     var tenantModelSelect = $("#modelselect");
     // Click events for the edit and delete buttons
-    $(document).on("submit", "#tsearch", tenantSearch);
+    //$(document).on("submit", "#tsearch", tenantSearch);
 
     // tenantModelSelect.on("change", handleModelChange);
-   
-
-    
-
+    // On click function for generatedCode.html
     function tenantSearch(event) {
-        event.preventDefault();
-        // Don't do anything if the name fields hasn't been filled out
-        if (!nameInput.val()) {
-          return;
-        }
-        // Calling the upsertAuthor function and passing in the value of the name input
-        getTenants({
-          name: nameInput
-            .val()
-            .trim()
-        });
-      }    // $(document).on("click", "button.delete", deleteTenant);
-    
+    if (!nameInput.val()) {
+      return;
+    }
+    // Calling the getTenants function and passing in the value of the name input
+      getTenants({
+        name: nameInput
+        .val()
+        .trim()
+      });
+    } 
 
     function getTenants() {
-        $.get("/api/tenants", function(data) {
-          var rowsToAdd = [];
-          for (var i = 0; i < data.length; i++) {
-            rowsToAdd.push(createTenantRow(data[i]));
-          }
-          renderTenantList(rowsToAdd);
-          nameInput.val("");
-        });
+      $.get("/api/tenants", function(data) {
+      var rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        rowsToAdd.push(createTenantRow(data[i]));
       }
+      renderTenantList(rowsToAdd);
+      nameInput.val("");
+      });
+    }
 
-      function renderTenantList(rows) {
-        tenantList.children().not(":last").remove();
-        tenantContainer.children(".alert").remove();
-        if (rows.length) {
-          console.log(rows);
-          tenantList.prepend(rows);
-        }
-        else {
-          renderEmpty();
-        }
+    function renderTenantList(rows) {
+      tenantList.children().not(":last").remove();
+      tenantContainer.children(".alert").remove();
+      if (rows.length) {
+        console.log(rows);
+        tenantList.prepend(rows);
       }
-    
-      // Function for creating a new list row for authors
-      function createTenantRow(tenantData) {
-        console.log(tenantData);
-        var newTr = $("<tr>");
-        newTr.data("tenant", tenantData);
-        newTr.append("<td>" + tenantData.name + "</td>");
-        newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
-        newTr.append("<td><a href='/lease?tenant_id=" + tenantData.id + "'>View Leasing Information</a></td>");
-        newTr.append("<td><a href='/bills?tenant_id=" + tenantData.id + "'>View Billing Information</a></td>");
-        newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
-        return newTr;
+      else {
+        renderEmpty();
       }
-    });
+    }
+
+    // Function for creating a new list row for authors
+    function createTenantRow(tenantData) {
+      console.log(tenantData);
+      var newTr = $("<tr>");
+      newTr.data("tenant", tenantData);
+      newTr.append("<td>" + tenantData.name + "</td>");
+      newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
+      newTr.append("<td><a href='/lease?tenant_id=" + tenantData.id + "'>View Leasing Information</a></td>");
+      newTr.append("<td><a href='/bills?tenant_id=" + tenantData.id + "'>View Billing Information</a></td>");
+      newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
+      return newTr;
+    }
+
+  $("#tsearch").on("click", function () {
+    console.log("hello world");  
+    // $(document).on("click", "button.delete", deleteTenant);
+    tenantSearch();
+    getTenants();
+    renderTenantList();
+    createTenantRow();
+  });
+}); // end generateCodeBtn function 
