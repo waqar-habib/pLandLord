1723eda45d21ee160ed632e9025de9da56a76fb6
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-06-21 20:32:33 -0500

    add tenant func works like a charm

--- public/js/newtenant.js
+++ public/js/newtenant.js
@@ -1,54 +1,43 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    function submitTenant(newGuy) {
+      $.post("/api/tenants/", newGuy, function(data) {
+        console.log("response back to front end was", data);
+        $("#modalContactForm").modal("show");
+        $("#modalContactForm").modal("hide");
+      });
+    }
+
     $(document).on("click", ".add-new-tenant", showAddTenantModal);
-    
-    
-        // Trigger Tenant Modal Form Function
-        function showAddTenantModal() {
-            $("#modalContactForm").modal("show");
-            $(document).on("click", ".submit-tenant", createNewTenant);
 
+    // Trigger Tenant Modal Form Function
+    function showAddTenantModal() {
+    $("#modalContactForm").modal("show");
+    };
 
-            function createNewTenant() {  
-                console.log("Hello World")
-                var nameInput = $("#name");
-                var emailInput = $("#email");
-                var dobInput = $("#dob")
-                var phonenumberInput = $("#phonenumber");
-                var moveindateInput = $("#move-in");
-                var moveoutdateInput = $("#move-out");
-                
-            
-                $("#modalContactForm").on("submit", function handleFormSubmit(event) {
-                    event.preventDefault();
-                        if (!nameInput.val().trim()) {
-                            return;
-                        }
-                        // Constructing a newPost object to hand to the database
-                        var newTenant = {
-                        name: nameInput.val().trim(),
-                        email_address: emailInput.val().trim(),
-                        date_of_birth: dobInput.val(),
-                        phone_number: phonenumberInput.val().trim(),
-                        move_in_date: moveindateInput.val().trim(),
-                        move_out_date: moveoutdateInput.val().trim(),
-                        email: emailInput.val().trim(),
-                        };
-                            console.log(newTenant);
-                            submitTenant(newTenant);
-    
-    
-                            
-                    
-                        });
-                function submitTenant() {
-                $.post("/api/tenants/", Tenant, function() {
-                });
-            }
-        };
-        };
-
-
-       
-
-
-});
+    $(document).on("click", ".submit-tenant", function(event) {
+      event.preventDefault();
+      console.log("click");
+      var newTenant = {
+        name: $("#name")
+          .val()
+          .trim(),
+        email_address: $("#email")
+          .val()
+          .trim(),
+        date_of_birth: $("#dob")
+          .val()
+          .trim(),
+        phone_number: $("#phone")
+          .val()
+          .trim(),
+        move_in_date: $("#in")
+          .val()
+          .trim(),
+        move_out_date: $("#out")
+          .val()
+          .trim()
+      };
+      console.log(newTenant);
+      submitTenant(newTenant);
+    });
+  });
